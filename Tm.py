d29c9b102d36f4261f2e28837ce6c52f1695678e
Trần Đoàn Thanh Sang <nguoidungkten@gmail.com>
2023-03-11 16:32:56 +0700

    K đính chung camera dc

--- Tm.py
+++ Tm.py
@@ -1,6 +1,11 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
+from tkinter import font
+import tkinter
+from tkinter import *
+from tkinter.ttk import *
+import PIL.Image, PIL.ImageTk
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -11,41 +16,77 @@ model = load_model(".\keras_model.h5", compile=False)
 # Load the labels
 class_names = open(".\labels.txt", "r").readlines()
 
-# CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture(0)
 
-while True:
-    # Grab the webcamera's image.
-    ret, image = camera.read()
+def giaodien():
+                              
+    root=Tk()
+    root.title("Nhận Diện")
+    root.geometry("400x400")
+    root.config(bg="lightcyan")
+    button_frame= Frame(root).pack(side=BOTTOM)  
+      
+    def nhandien():
+        
+        while True:
+            # Grab the webcamera's image.
+            ret, image = camera.read()
+            # Resize the raw image into (224-height,224-width) pixels
+            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)           
+            # Show the image in a window
+            cv2.imshow("Webcam Image", image)
+            
+            # Make the image a numpy array and reshape it to the models input shape.
+            image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+            # Normalize the image array
+            image = (image / 127.5) - 1
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+            # Predicts the model
+            prediction = model.predict(image)
+            index = np.argmax(prediction)
+            class_name = class_names[index]
+            confidence_score = prediction[0][index]
 
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+            # Print prediction and confidence score  
+                      
+            textvat="".join(["Vật:",class_name[2:]])
+            textdtc="".join(["Độ tin cậy:",str(np.round(confidence_score * 100))[:-2],"%" ])                           
+            print("Class:", class_name[2:], end="")
+            print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")            
+            # Listen to the keyboard for presses.           
+            keyboard_input = cv2.waitKey(1)            
+            # 27 is the ASCII for the esc key on your keyboard.          
+            if keyboard_input == 27:                                              
+                break 
+        update_frame(textvat,textdtc)
+        cv2.destroyAllWindows()
+       
+                       
+    def update_frame(textvat,textdtc):
+        vat.config(text=textvat)
+        dotincay.config(text=textdtc)
+ 
+    lable = tkinter.Label(root,text="Ấn Nhận Diện để bắt đầu",fg="black",bd=0,bg="lightcyan" )
+    lable.config(font=("",10))
+    lable.pack(pady=0)
+    
+    lable1 = tkinter.Label(root,text="Ấn ESC để nhận kết quả",fg="black",bd=0,bg="lightcyan" )
+    lable1.config(font=("",10))
+    lable1.pack(pady=0)
+    
+    ND = tkinter.Button(button_frame,text="Nhận Diện",font=(("Arial"),10,'bold'),bg="#303030",fg="#FFFFFF",command=nhandien )
+    ND.pack(pady=10)
+    
+    vat = tkinter.Label(root,text="",fg="black",bd=0,bg="lightcyan")
+    vat.config(font=("",10))
+    vat.pack(pady=0)
+    
+    dotincay = tkinter.Label(root,text="",fg="black",bd=0,bg="lightcyan")
+    dotincay.config(font=("",10))
+    dotincay.pack(pady=0)
+    
+    root.mainloop()
 
-    # Normalize the image array
-    image = (image / 127.5) - 1
-
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-
-    # Print prediction and confidence score
-    print("Vật:", class_name[2:], end="")
-    print("Độ tin cậy:", str(np.round(confidence_score * 100))[:-2], "%")
-
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
-
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
-        break
-
-camera.release()
-cv2.destroyAllWindows()
+giaodien()
+camera = cv2.VideoCapture(0)
